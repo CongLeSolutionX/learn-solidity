c7773d62d0ae3a86f667aa8a8a79d0f0ac9fd127
CongLe <longchike@gmail.com>
2024-01-14 12:22:04 -0500

    - Sload and locations

--- 1b-storage-variables/examples/0-example-storage-slots/src/Example.sol
+++ 1b-storage-variables/examples/0-example-storage-slots/src/Example.sol
@@ -4,25 +4,29 @@ pragma solidity ^0.8.20;
 import "forge-std/console.sol";
 
 contract Example {
-    uint256 a = 100; // storage slot - 0x0
-    uint256 b;       // storage slot - 0x1
-    bool c;          // storage slot - 0x2
+    uint256 a = 100;                  // storage slot - 0x0
+    uint256 b = type(uint256).max;    // storage slot - 0x1
+    bool c;                           // storage slot - 0x2
 
     constructor() {
         // SSTORE - store to some storage location
         // SLOAD - read from some storage location
-        // bool x;
-        // assembly {
-        //     x := sload(0x2)
-        // }
-        //console.log(x);
-
         uint256 x;
+        bytes32 y;
+
+        bytes32 uint256HexFormMaxValue;
+        uint256 uint256DecimalFormMaxValue;
         assembly {
             x := sload(0x0)
+            y := sload(0x0)
+            uint256HexFormMaxValue := sload(0x1)
+            uint256DecimalFormMaxValue := sload(0x1)
         }
-        console.log("x: %s", x); // x should return the same value as a at location 0x0
-
+        console.log("Decimal value of x at 0x0: %s", x); // x should return the same value as a at location 0x0
+        console.logBytes32(y); // y should return the value of a at location 0x0 in hex format
         console.log("a: %s", a);
+
+        console.logBytes32(uint256HexFormMaxValue); // return hex value of 2^256 - 1
+        console.log("uint256DecimalFormMaxValue: %s", uint256DecimalFormMaxValue); // return decimal value of 2^256 - 1
     }
 }
