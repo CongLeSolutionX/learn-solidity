7ca3aced3234007681674f229ba598044f19a6e9
CongLe <longchike@gmail.com>
2024-01-14 20:25:53 -0500

    - add documentation

--- 2-sending-ether/examples/0-send-ether/src/Receiver-ether.sol
+++ 2-sending-ether/examples/0-send-ether/src/Receiver-ether.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "forge-std/console.sol";
+
 contract Contract {
-    address public owner;
+    address public owner; // The owner of the contract
 
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender; // msg.sender is the address that is calling the function
+    }
+
+    // receive() is a special function that is executed when the contract receives ether.
+    // It is executed when the contract receives ether without any calldata.
+    receive() external payable {
+        console.log(msg.value);
     }
 }
